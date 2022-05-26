777e4a36849749e428ae3e6e8f282b20acf337bc
Dmitrii K <dm-kamaev@rambler.ru>
2022-05-26 17:27:03 +0300

    fix bug with export

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.js' // <--- Will be compiled to this single file
+    filename: 'index.js', // <--- Will be compiled to this single file,
+    libraryExport: 'default',
   },
   resolve: {
     extensions: ['.js'],
